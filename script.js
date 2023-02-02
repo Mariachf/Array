a9e69dabc0ab0f394cb2c0f73e479008df2da009
Maria Eduarda <80916152+Mariachf@users.noreply.github.com>
2023-02-02 11:58:47 -0300

    AT-ARRAY

---
+++ script.js
@@ -0,0 +1,89 @@
+1
+/* let frutas = ["morango", "banana", "laranja", "uva", "pêssego"]
+console.log(frutas)
+ */
+
+2
+/* let frutas = ["morango", "banana", "laranja", "uva", "pêssego"]
+frutas.unshift("Jaca");
+console.log(frutas); */
+
+3
+/* let frutas = ["morango", "banana", "laranja", "uva", "pêssego"]
+frutas.pop();
+console.log(frutas); */
+
+4
+/* 
+let frutas = ["morango", "banana", "laranja", "uva", "pêssego"]
+frutas.push("tangerina", "goiaba")
+console.log(frutas) */
+
+5
+/* let frutas = ["morango", "banana", "laranja", "uva", "pêssego"]
+frutas.shift()
+console.log(frutas) */
+
+6
+/* let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort(function(a, b) {
+    return (a-b);
+    });
+    console.log(numbers) */
+
+7
+/* const alunos = {
+    aluno1 : 'marcelo',
+     aluno2 : 'paulo',
+    aluno3 : 'rau',
+}
+
+console.log(alunos) */
+
+8
+/* const alunos = {
+    aluno1 : 'marcelo',
+     aluno2 : 'paulo',
+    aluno3 : 'rau',
+}
+
+alunos.anulo4 = 'danilo'
+
+console.log(alunos) */
+
+9
+
+/* 
+const alunos = {
+    aluno1 : 'marcelo',
+     aluno2 : 'paulo',
+    aluno3 : 'rau',
+}
+
+delete alunos.aluno2;
+console.log(alunos) */
+
+10
+
+/*  const alunos = {
+    aluno1 : 'marcelo',
+     aluno2 : 'paulo',
+    aluno3 : 'rau',
+}
+
+
+console.log(alunos.aluno1)
+console.log(alunos.aluno2)
+console.log(alunos.aluno3)
+ */
+
+11,12
+
+/* const cadastro = {
+    nome : 'josefa',
+    idade : 25,
+    telefone : 119618523,
+    amigos : ['rau', 'ruan', 'renato', 'roberta'],
+}
+
+console.log(cadastro.amigos) */
